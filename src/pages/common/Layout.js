398a3cd891eb89153b6a3fb4907ad54c94fdb92c
kdg010729 <kdg010729@naver.com>
2025-06-25 09:39:00 +0900

    fix

--- src/pages/common/Layout.js
+++ src/pages/common/Layout.js
@@ -1,5 +1,5 @@
 import Header from "./Header";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 import {Outlet} from "react-router-dom";
 import Footer from "./Footer";
 import {useEffect} from "react";
